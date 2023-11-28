d690e1c4c67d92748dc1a10e9d474b232039fcbc
Alicia Garrido-Peña <aliciagp96@gmail.com>
2023-11-28 11:29:27 +0100

    Logger and Waveform.npy

--- cluster_identification.py
+++ cluster_identification.py
@@ -30,8 +30,32 @@ from sssio import *
 
 import matplotlib.pyplot as plt 
 
-# banner
-# print(banner)
+# logging
+import logging
+
+log_fmt = "%(asctime)s - %(levelname)s - %(message)s"
+date_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter(log_fmt, datefmt=date_fmt)
+
+# for printing to stdout
+logger = logging.getLogger()  # get all loggers
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+logging.getLogger('functions').setLevel(logging.INFO)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
+# logging unhandled exceptions
+
+
+def handle_unhandled_exception(exc_type, exc_value, exc_traceback):
+    # TODO make this cleaner that it doesn't use global namespace
+    logging.critical("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = handle_unhandled_exception
+
 
 #Load file
 
@@ -40,7 +64,7 @@ config_path = Path(os.path.abspath(sys.argv[1]))
 sssort_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 Config = configparser.ConfigParser()
 Config.read(config_path)
-print_msg('config file read from %s' % config_path)
+logger.info('config file read from %s' % config_path)
 
 mpl.rcParams['figure.dpi'] = Config.get('output','fig_dpi')
 fig_format = Config.get('output','fig_format')
@@ -59,6 +83,13 @@ plots_folder = results_folder / 'plots_post'
 
 os.makedirs(plots_folder, exist_ok=True)
 
+# config logger for writing to file
+file_handler = logging.FileHandler(filename="%s.log" % exp_name, mode='w')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+# Load clustering data
 Blk = get_data(results_folder  / "result.dill")
 
 SpikeInfo = pd.read_csv(results_folder / "SpikeInfo.csv")
@@ -67,7 +98,7 @@ unit_column = [col for col in SpikeInfo.columns if col.startswith('unit')][-1]
 SpikeInfo = SpikeInfo.astype({unit_column: str})
 units = get_units(SpikeInfo,unit_column)
 
-#Load Templates
+#Load Waveforms
 Waveforms = np.load(results_folder / "Waveforms.npy")
 fs = Blk.segments[seg_no].analogsignals[0].sampling_rate
 n_samples = np.array(Config.get('postprocessing', 'template_window').split(','), dtype='float32')/1000.0
@@ -76,7 +107,7 @@ n_samples = np.array(n_samples * fs, dtype= int)
 new_column = 'unit_labeled'
 
 if new_column in SpikeInfo.keys():
-    print_msg("Clusters already assigned")
+    logger.info("Clusters already assigned")
     print(SpikeInfo[unit_column].value_counts())
     exit()
 
@@ -109,9 +140,7 @@ template_A = template_A[tmid_a-left:tmid_a+right]
 template_B = template_B[tmid_b-left:tmid_b+right]
 Waveforms = Waveforms[n_samples[0]-left:n_samples[0]+right,:]
 
-
-
-print_msg("Current units: %s"%units)
+logger.info("Current units: %s"%units)
 
 distances_a = []
 distances_b = []
@@ -119,7 +148,7 @@ means = []
 
 mode = 'peak'
 
-print_msg("Computing best assignment")
+logger.info("Computing best assignment")
 #Compare units to templates
 mean_waveforms = {}
 amplitude = []
@@ -144,12 +173,12 @@ for unit in units:
     distances_b.append(d_b)
     means.append(mean_waveforms[unit]*norm_factor)
 
-print_msg("Distances to a: ")
-print_msg("\t\t%s" % str(units))
-print_msg("\t\t%s" % str(distances_a))
-print_msg("Distances to b: ")
-print_msg("\t\t%s" % str(units))
-print_msg("\t\t%s" % str(distances_b))
+logger.info("Distances to a: ")
+logger.info("\t\t%s" % str(units))
+logger.info("\t\t%s" % str(distances_a))
+logger.info("Distances to b: ")
+logger.info("\t\t%s" % str(units))
+logger.info("\t\t%s" % str(distances_b))
 
 # Get best assignments
 a_unit = units[np.argmin(distances_a)]
@@ -161,7 +190,7 @@ if len(units) > 2:
 asigs = {a_unit: 'A', b_unit: 'B'}
 if len(units) > 2:
     asigs[non_unit]= '?'
-print_msg("Final assignation: %s" % asigs)
+logger.info("Final assignation: %s" % asigs)
 
 # plot assignments
 outpath = plots_folder / ("cluster_reassignments" + fig_format)
@@ -180,5 +209,5 @@ SpikeInfo.loc[b_unit_rows.index, new_column] = 'B'
 
 # store SpikeInfo
 outpath = results_folder / 'SpikeInfo_post.csv'
-print_msg("saving SpikeInfo to %s" % outpath)
+logger.info("saving SpikeInfo to %s" % outpath)
 SpikeInfo.to_csv(outpath,index= False)
