4c72ae5df3f2d0d5f73cb81342bf674d95c764e1
Alicia  Garrido <aliciagp96@gmail.com>
2023-11-30 17:34:12 +0100

    Error message when no spikes are detected (z-score different threshold selection

--- sssort.py
+++ sssort.py
@@ -158,6 +158,10 @@ for i, seg in enumerate(Blk.segments):
     st = spike_detect(AnalogSignal, global_mad * mad_thresh, min_prominence)
     st.annotate(kind='all_spikes')
 
+    if len(st) == 0:
+        logger.error("No spikes detected, please enter check the threshold values in the configuration file")
+        exit()
+
     # remove border spikes
     st_cut = st.time_slice(st.t_start + wsize / 2, st.t_stop - wsize / 2)
     st_cut.t_start = st.t_start
